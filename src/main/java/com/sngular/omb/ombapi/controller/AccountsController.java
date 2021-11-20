55d70c535649fde9805c34a4d10efa1739a7de1a
Leandro Guillen <leandroguillen@gmail.com>
2021-11-20 01:17:47 -0500

    enable cors for both endpoints

--- src/main/java/com/sngular/omb/ombapi/controller/AccountsController.java
+++ src/main/java/com/sngular/omb/ombapi/controller/AccountsController.java
@@ -5,12 +5,14 @@ import com.sngular.omb.ombapi.service.AccountsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class AccountsController {
-
+        
     @Autowired
     private AccountsService accountsService;
 
--- src/main/java/com/sngular/omb/ombapi/controller/DepositController.java
+++ src/main/java/com/sngular/omb/ombapi/controller/DepositController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class DepositController {
 
     @Autowired
